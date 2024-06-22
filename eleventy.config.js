8f7411e2d6330d98738f4b81725399dccce7a32c
Rizaldi Martaputra <zal@fastmail.com>
2024-06-22 14:34:19 +0700

    Add tag and date to post

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,9 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const Image = require("@11ty/eleventy-img");
+const dayjs = require('dayjs');
+const customParseFormat = require("dayjs/plugin/customParseFormat");
+
+dayjs.extend(customParseFormat);
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
@@ -20,6 +24,16 @@ module.exports = function(eleventyConfig) {
 
     // You bet we throw an error on a missing alt (alt="" works okay)
     return Image.generateHTML(metadata, imageAttributes);
+  });
+
+  eleventyConfig.addFilter("postTags", function(value) {
+    return value.filter(item => item != 'post');
+  });
+
+  eleventyConfig.addFilter("postDate", function(value) {
+    console.log(value);
+    console.log(typeof value);
+    return dayjs(value).format('dddd, DD MMMM YYYY');;
   });
 
   return {
